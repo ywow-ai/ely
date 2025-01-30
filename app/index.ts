11428ca2513ec61b6ae05802071c495a42327e76
ywow-ai <masyaqin37@gmail.com>
2025-01-30 13:22:23 +0700

    feat: add JWT, Swagger, and logging middleware to Elysia app

--- app/index.ts
+++ app/index.ts
@@ -2,27 +2,13 @@ import { Elysia } from "elysia";
 import { node } from "@elysiajs/node";
 import { swagger } from "@elysiajs/swagger";
 import { jwt } from "@elysiajs/jwt";
-import logixlysia from "logixlysia";
 
 const port: number = 3000;
 
 new Elysia({ adapter: node() })
   .use(
-    logixlysia({
-      config: {
-        showStartupMessage: true,
-        startupMessageFormat: "banner",
-        timestamp: {
-          translateTime: "yyyy-mm-dd HH:MM:ss",
-        },
-        ip: true,
-        logFilePath: "./logs/runtime.log",
-        customLogFormat:
-          "{now} {level} {duration} {method} {pathname} {status} {message} {ip} {epoch}",
-      },
-    })
+    jwt({ name: "jwt", secret: process.env.JWT_SECRETS ?? "RWX", exp: "1h" })
   )
-  .use(jwt({ name: "jwt", secret: process.env.JWT_SECRETS ?? "RWX", exp: "1h" }))
   .use(swagger())
   .get("/", () => "Hello Node!")
   .listen(port);
