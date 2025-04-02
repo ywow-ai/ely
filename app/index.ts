d9f58780ee84e0ab6f3bebcaa686da97f6fbcfcc
ywow-ai <masyaqin37@gmail.com>
2025-04-02 20:30:37 +0700

    adjust session expired

--- app/index.ts
+++ app/index.ts
@@ -3,12 +3,17 @@ import { node } from "@elysiajs/node";
 import { jwt } from "@elysiajs/jwt";
 
 const port: number = 3000;
+const session_expired: number = 3600;
 
 new Elysia({ adapter: node() })
   .use(
-    jwt({ name: "jwt", secret: process.env.JWT_SECRETS ?? "RWX", exp: "1h" })
+    jwt({
+      name: "jwt",
+      secret: process.env.JWT_SECRETS ?? "RWX",
+      exp: new Date(Date.now() + session_expired * 1000).getTime(),
+    })
   )
-  .get("/", () => "Hello Node!")
+  .get("/", () => ({ message: "service is running" }))
   .get("/data", () => [
     { id: 1, name: "Alice", age: 25 },
     { id: 2, name: "Bob", age: 30 },
